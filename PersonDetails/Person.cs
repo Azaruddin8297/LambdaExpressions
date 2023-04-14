3c7c8341457f1f1888d353196c10a2ad4c544568
Azaruddin8297 <ajarpopo.com>
2023-04-14 23:19:57 +0530

    Lambda

---
+++ PersonDetails/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDetails
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string SSN, string Name, string Address,int Age)
+        {
+            this.SSN = SSN;
+            this.Name = Name;
+            this.Address = Address;
+            this.Age = Age;
+        }
+    }
+}
